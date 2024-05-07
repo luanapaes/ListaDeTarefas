df722fc3b56a22bf0c8df2b18b513fbc0445ba51
Luana Paes <luanafranca598@gmail.com>
2024-05-06 21:20:38 -0300

    Adicionando lógica ao componente input-add

---
+++ lista-de-tarefas/src/app/modules/to-do-list/components/input-add-item/input-add-item.component.ts
@@ -0,0 +1,38 @@
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Output, ViewChild, inject, signal } from '@angular/core';
+import { IListItems } from '../../interface/IListItems.interface';
+
+@Component({
+  selector: 'app-input-add-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-add-item.component.html',
+  styleUrl: './input-add-item.component.scss'
+})
+export class InputAddItemComponent {
+  #cdr = inject(ChangeDetectorRef);
+
+  @ViewChild("inputText") public inputText!: ElementRef; // acessa o elemento HTML que possui esse identificador
+  
+  @Output() public outputItems = new EventEmitter<IListItems>();
+  
+  public focusAndAddItem(value: string){
+    if(value){
+      this.#cdr.detectChanges(); // monitora a mudança de valor dentro do input
+      this.inputText.nativeElement.value = ''; // limpa o input após o "envio"
+
+      // gera o id dos items
+      const dataAtual = new Date();
+      const timestamp = dataAtual.getTime();
+      const id = `ID ${timestamp}`;
+
+      // envia para fora estas propriedades
+      this.outputItems.emit({
+        id,
+        checked: false,
+        value
+      })
+
+      return this.inputText.nativeElement.focus(); // após o "envio" o foco volta para o input.
+    }
+  }
+}
